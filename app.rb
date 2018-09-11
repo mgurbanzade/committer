3f41a91bbc46b37e83bfd3ffd2032a02f2d43361
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-09-11 15:35:52 +0400

    Create commit methods

--- app.rb
+++ app.rb
@@ -1,22 +1,39 @@
 class App
-  attr_reader :username, :repo_name
+  attr_reader :repo_name, :file_name
 
   def initialize
-    ask_for_username
-    ask_for_repo_name
-    @link = "https://github.com/#{@username}/#{@repo_name}"
-    puts 'That\'s it! Committer will commit to your repo everyday unless you stop it.'
-    puts 'To stop committing use corresponding option from the menu.'
+    @repo_name = 'github_committer'
+    @file_name = 'committer.txt'
+    navigate_to_repo
+    prepare_file
+    commit
+    push
   end
 
-  def ask_for_username
-    puts 'Your github username:'
-    @username = gets.chomp
+  def navigate_to_repo
+    Dir.chdir
+    return Dir.chdir(repo_name) if Dir.exist?(repo_name)
+
+    Dir.mkdir(repo_name)
+    Dir.chdir(repo_name)
+    system "git init"
   end
 
-  def ask_for_repo_name
-    puts 'Your repository name:'
-    @repo_name = gets.chomp
+  def prepare_file
+    system "touch #{file_name}" unless File.exist?(file_name)
+    File.truncate(file_name, 0) if File.empty?(file_name)
+    File.write(file_name, Time.now)
+  end
+
+  def commit
+    system "git add ."
+    commit_message = "#{Time.now.strftime("%Y%m%d")}_commit"
+    system "git commit -m '#{commit_message}'"
+  end
+
+  def push
+    system "hub create"
+    system "git push -u origin master"
   end
 end
 
